045c4a0cb1562a555346f588d980d3e1cbf07520
combofish <18233092052@139.com>
2019-12-06 11:51:39 +0800

    script to install i3, urxvt, and configuration.

--- startup.sh
+++ startup.sh
@@ -1,7 +1,44 @@
 #!/usr/bin/sh
 
-ln .vimrc ~/.vimrc
-ln .shrc ~/.shrc
-ln .zshrc ~/.zshrc
+# ln .vimrc ~/.vimrc
+# ln .shrc ~/.shrc
+# ln .zshrc ~/.zshrc
 
-ln -s emacs.d ~/.emacs.d
+# ln -s emacs.d ~/.emacs.d
+
+config_pre_i3(){
+    deps=(conky xscreensaver feh shutter rofi konsole i3status i3blocks xcompmgr)
+    sudo apt-get install ${deps[@]}
+}
+
+config_i3blocks(){
+    mkdir ~/.config/i3blocks
+}
+
+config_i3(){
+    if [ -f ~/.config/i3/config ]; then
+        mv ~/.config/i3/config ~/.config/i3/config-$(date +"%m-%d-%Y").bak
+    fi
+
+    ln ./i3/config ~/.config/i3/config
+}
+
+config_urxvt(){
+    # download
+    # sudo apt-get install rxvt-unicode
+    echo "config urxvt"
+
+    if [ -f ~/.Xresources ]; then
+        mv ~/.Xresources ~/.Xresources-$(date +"%m-%d-%Y").bak
+    fi
+
+    ln ./.Xresources ~/.Xresources
+}
+
+config_system(){
+    # config_pre_i3
+    # config_i3
+    config_urxvt
+}
+
+config_system
